706edabee96eb8207f8c60dea1928d1f9b7fa5eb
Connecter95 <huat12huat12@outlook.com>
2026-08-28 22:05:36 +0800

    add factories for Category, Product, and Supplier models with default states

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,26 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Supplier;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $categories = Category::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $suppliers = Supplier::factory(10)->create();
+
+        Product::factory(50)
+            ->recycle($categories)
+            ->create()
+            ->each(function (Product $product) use ($suppliers) {
+                $product->suppliers()->attach(
+                    $suppliers->random(rand(1, 3))->pluck('id')
+                );
+            });
     }
 }
